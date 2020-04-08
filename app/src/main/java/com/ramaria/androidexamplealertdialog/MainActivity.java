5e3cd4be4aa256953d1e02b0d11d6f4c99a7e91b
Raquel Maria <45218570+RaquelMariah@users.noreply.github.com>
2020-04-08 11:46:47 -0300

    Adicionado toast no dialog

--- app/src/main/java/com/ramaria/androidexamplealertdialog/MainActivity.java
+++ app/src/main/java/com/ramaria/androidexamplealertdialog/MainActivity.java
@@ -1,9 +1,12 @@
 package com.ramaria.androidexamplealertdialog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,24 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void abrirDialog(View view){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("O dialog foi aberto");
+        dialog.setMessage("Esse dialog foi útil?");
+        dialog.setCancelable(false);
+        dialog.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(),"O dialog foi útil", Toast.LENGTH_SHORT).show();
+            }
+        });
 
+            dialog.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(getApplicationContext(),"O dialog não foi útil", Toast.LENGTH_SHORT).show();
+                }
+            });
+            dialog.create();
+            dialog.show();
     }
 }
